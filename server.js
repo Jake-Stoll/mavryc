f785f6a6f78960587970e71abed6fcea43046bbd
Jake <jake.r.stoll@outlook.com>
2017-04-07 12:06:45 -0500

    Create new user http

--- server.js
+++ server.js
@@ -225,3 +225,15 @@ app.post('/flight-search', (req, res) => {
 
 
 
+//Authentication 
+
+// app.post('/login',
+//   passport.authenticate('local'),
+//   function(req, res) {
+//     // If this function gets called, authentication was successful.
+//     // `req.user` contains the authenticated user.
+//     res.redirect('/users/' + req.user.username);
+//   });
+
+
+
