645546a453c2a24a8cd80f9a1c5df4ace5f248bc
Jake <jake.r.stoll@outlook.com>
2017-04-10 22:00:49 -0700

    Updates to Facebook Auth. Not fully complete.

---
+++ config/auth.js
@@ -0,0 +1,11 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports so we can use it!
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1920729421510515', // FB App ID
+        'clientSecret'  : '4466085a1dd8fa99d46a8a02d20a424a', // FB App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    }
+};
