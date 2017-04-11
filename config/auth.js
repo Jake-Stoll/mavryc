6d42f746c23f9a9aab88f188ada2c43a4c1b2611
Jake <jake.r.stoll@outlook.com>
2017-04-11 13:22:47 -0700

    Facebook login and register is now complete.

--- config/auth.js
+++ config/auth.js
@@ -6,6 +6,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1920729421510515', // FB App ID
         'clientSecret'  : '4466085a1dd8fa99d46a8a02d20a424a', // FB App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
     }
 };
