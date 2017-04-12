eaf9524c59567896ec8e6f520aee93c954e01825
Jake <jake.r.stoll@outlook.com>
2017-04-11 18:48:52 -0700

    Another test with FB login and Heroku.

--- config/auth.js
+++ config/auth.js
@@ -3,9 +3,12 @@
 // expose our config directly to our application using module.exports so we can use it!
 module.exports = {
 
+    //Need to write a function to offer different callbackURL based on dev environment (testing vs. production)
     'facebookAuth' : {
         'clientID'      : '1920729421510515', // FB App ID
         'clientSecret'  : '4466085a1dd8fa99d46a8a02d20a424a', // FB App Secret
         'callbackURL'   : 'https://mavryc.herokuapp.com//auth/facebook/callback'
     }
 };
+
+
